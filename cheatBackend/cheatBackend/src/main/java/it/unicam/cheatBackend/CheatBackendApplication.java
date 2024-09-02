9dea0bd7e1576a0b43becb4c94f6f26b652aa87e
alessandrofrontini <150081100+alessandrofrontini@users.noreply.github.com>
2024-09-03 01:00:25 +0200

    start backend

---
+++ cheatBackend/cheatBackend/src/main/java/it/unicam/cheatBackend/CheatBackendApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cheatBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheatBackendApplication.class, args);
+	}
+
+}
---
+++ cheatBackend/cheatBackend/src/test/java/it/unicam/cheatBackend/CheatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.cheatBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
